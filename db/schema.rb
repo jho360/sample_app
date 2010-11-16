95a7799d44a636fe47d6a8f974d49f77b54779fa
Justin Ho <justinho1@gmail.com>
2010-11-15 19:57:02 -0500

    User model validation (I hope to god I got this right)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101115225626) do
+ActiveRecord::Schema.define(:version => 20101116005217) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
