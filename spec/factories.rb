52f1ca3bfbd2dcb5942d25e740844a1269007434
Justin Ho <justinho1@gmail.com>
2010-11-23 21:02:46 -0500

    Added a User factory and test for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+Factory.define :user do |user|
+  
+  user.name                   "Jason Ho"
+  user.email                  "J@ho.com"
+  user.password               "public"
+  user.password_confirmation  "public"
+  
+end
